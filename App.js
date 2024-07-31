4f51b00b3e468c9a792a51481bace2d08a0ec18e
kaylonm17 <kaylonmyers2001@gmail.com>
2024-07-31 10:19:52 -0500

    tinder swipe feature

--- App.js
+++ App.js
@@ -1,67 +1,138 @@
-import React from "react";
-import { Text, Image, View, StyleSheet, ImageBackground } from 'react-native';
+import 'react-native-gesture-handler';
+import React, {useState, useEffect,} from "react";
+import {View, StyleSheet, Text, Pressable, useWindowDimensions, Image,} from 'react-native';
+import Card from './src/components/TinderCard';
+import users from './assets/data/users';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, useAnimatedGesture, withDecay, useDerivedValue, interpolate, runOnJS, } from  'react-native-reanimated'
+import { Gesture, GestureDetector, GestureHandlerRootView, } from 'react-native-gesture-handler';
+import { AnimatedView } from 'react-native-reanimated/lib/typescript/component/View';
+import Like from './assets/images/LIKE.png';
+import Nope from './assets/images/nope.png';
+ 
+const SIZE = 120;
+
+const ROTATION = 60;
+const SWIPE_VELOCITY = 800;
 
 const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
+
+  const currentProfile = users[currentIndex];
+  const nextProfile =  users[nextIndex];
+
+  const {width: screenWidth} = useWindowDimensions();
+
+  const hiddenTranslateX = 2 * screenWidth;
+
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(() => interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION])+ 'deg',);
+
+
+  const pan = Gesture.Pan()
+  .onChange((event) => {
+    translateX.value += event.changeX;
+  })
+  .onFinalize((event) => {
+      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+        translateX.value = withSpring(0);
+        return;
+      }
+      translateX.value = withSpring(
+          hiddenTranslateX * Math.sign(event.velocityX),
+          {},
+          () => runOnJS(setCurrentIndex)(currentIndex+1)
+        );
+        
+
+  });
+
+  useEffect(() => {
+    translateX.value = 0
+    setNextIndex(currentIndex+1);
+  }, [currentIndex]);
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateX.value, },
+    { rotate: rotate.value, },
+    ],
+  })); 
+
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [ 
+      {scale: interpolate(translateX.value, [-hiddenTranslateX, 0, hiddenTranslateX], [1, 0.8, 1])},
+    ],
+      opacity: interpolate(
+        translateX.value, [-hiddenTranslateX, 0, hiddenTranslateX], [1, 0.5, 1],
+      ),
+  })); 
+
+   const likeStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(
+      translateX.value, [ 0, -hiddenTranslateX / 5], [0, 1],
+    ),
+   }));
+
+   const nopeStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(
+      translateX.value, [ 0, hiddenTranslateX / 5], [0, 1],
+    ),
+   }));
+
+
+  const gesture = Gesture.Tap().onBegin(() => {
+    console.log(_WORKLET);
+  });
+
  return (
-  <View style={styles.pageContainer}>
-     <View style={styles.card}>
-      <ImageBackground
-        source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'
-        }} 
-        style={styles.image}>
-          <View style={styles.cardInner}>
-            <Text style={styles.name}>Elon Musk</Text>
-            <Text style={styles.bio}>A dude with a rocket is looking for a gal with fuel</Text>
-          </View>
-        </ImageBackground>
+  <GestureHandlerRootView>
+    <View style={styles.pageContainer}>
+    {nextProfile && ( 
+      <View style={styles.nextCardContainer}>
+      <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+        <Card user={nextProfile}/> 
+      </Animated.View>
+      </View>
+      )}
+
+      {currentProfile && (
+        <GestureDetector gesture={pan}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            <Animated.Image source={Like} style={[styles.like, {left: 10}, likeStyle]} resizeMode="contain"/>
+            <Animated.Image source={Nope} style={[styles.like, {right: 10}, nopeStyle]}/> 
+            <Card user={currentProfile}/>
+          </Animated.View>
+        </GestureDetector>
+        )}
     </View>
-  </View>
+  </GestureHandlerRootView>
  );
 };
 const styles = StyleSheet.create({
   pageContainer: {
     justifyContent: 'center',
      alignItems: 'center',
-      flex: 1
+      flex: 1,
     },
-    card: {
-      width: '95%',
+    animatedCard: {
+      width: '90%',
       height: '70%',
-      borderRadius: 10,
-
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 5,
-      },
-      shadowOpacity: 0.36,
-      shadowRadius: 6.68,
-
-      elevation: 11,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
-    image: {
-      width: '100%',
-       height: '100%',
-       borderRadius: 10,
-       overflow: 'hidden',
-
-       justifyContent: 'flex-end',
-      },
-    cardInner: {
-      padding: 10,
+    nextCardContainer: {
+      ...StyleSheet.absoluteFillObject,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
-      name: {
-        fontSize: 30,
-        color: 'white',
-        fontWeight: 'bold',
-        
-      },
-      bio: {
-        fontSize: 18,
-        color: 'white',
-        lineHeight: 25,
-      },
+    like: {
+      width: 150,
+      height: 150,
+      position: 'absolute',
+      top: 10,
+      zIndex: 1,
+      elevation: 1,
+    },
 });
 
 export default App;
