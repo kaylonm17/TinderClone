e6a56e56b2fbb8782fe8f11155f8f1ef8146d980
kaylonm17 <kaylonmyers2001@gmail.com>
2024-08-04 13:16:53 -0500

    New matches

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
 import 'react-native-gesture-handler';
 import React from "react";
 import {View, StyleSheet} from 'react-native';
-import Card from './src/components/TinderCard';
-import users from './assets/data/users';
 import { GestureHandlerRootView, } from 'react-native-gesture-handler';
-import AnimatedStack from './src/components/AnimatedStack';
+import HomeScreen from './src/screens/HomeScreen';
+import MatchesScreen from './src/screens/MatchesScreen';
 
 const App = () => {
-
-  const onSwipeLeft = (user) => {
-    console.warn("swipe left", user.name)
-  };
-  const onSwipeRight = (user) => {
-    console.warn("swipe right", user.name)
-  };
-
 
  return (
   <GestureHandlerRootView>
     <View style={styles.pageContainer}>
-      <AnimatedStack
-        data={users}
-        renderItem={(({ item }) => <Card user={item}/> )}
-        onSwipeLeft={onSwipeLeft}
-        onSwipeRight={onSwipeRight}
-      />
+      <MatchesScreen/>
     </View>
   </GestureHandlerRootView>
  );
