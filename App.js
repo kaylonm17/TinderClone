a3337ea9ef3024da64c12d94f15da32a2090372a
kaylonm17 <kaylonmyers2001@gmail.com>
2024-08-18 12:54:41 -0500

    Icons and Navigation

--- App.js
+++ App.js
@@ -1,26 +1,61 @@
 import 'react-native-gesture-handler';
-import React from "react";
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from "react";
+import {View, StyleSheet, Text, SafeAreaView, Pressable} from 'react-native';
 import { GestureHandlerRootView, } from 'react-native-gesture-handler';
+
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
 
 const App = () => {
+  const [activeScreen, setActiveScreen] = useState('HOME');
 
+const color = "#b5b5b5";
+const activeColor = '#F76C6B';
  return (
   <GestureHandlerRootView>
+    <SafeAreaView style={styles.root}>
     <View style={styles.pageContainer}>
-      <MatchesScreen/>
+      <View style={styles.topNavigation}>
+        <Pressable onPress={() => setActiveScreen('HOME')}>
+          <Fontisto name="tinder" size={30} color={activeScreen == 'HOME' ? activeColor : color} />
+        </Pressable>
+        <Pressable onPress={() => setActiveScreen('LIKES')}>
+          <MaterialCommunityIcons name="star-four-points" size={30} color={activeScreen == 'LIKES' ? activeColor : color} />
+        </Pressable>
+        <Pressable onPress={() => setActiveScreen('CHAT')}>
+          <Ionicons name="chatbubbles" size={30} color={activeScreen == 'CHAT' ? activeColor : color} />
+        </Pressable>
+        <Pressable onPress={() => setActiveScreen('USER')}>
+          <FontAwesome name="user" size={30} color={activeScreen == 'USER' ? activeColor : color} />
+        </Pressable>
+      </View>
+      { activeScreen == 'HOME' && <HomeScreen/> }
+      { activeScreen == 'CHAT' && <MatchesScreen/>}
     </View>
+    </SafeAreaView>
   </GestureHandlerRootView>
  );
 };
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   pageContainer: {
     justifyContent: 'center',
      alignItems: 'center',
       flex: 1,
     },
+    topNavigation: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      width: '100%',
+      padding: 10 ,
+    }
 });
 
 export default App;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,11 @@ import {View, StyleSheet} from 'react-native';
 import Card from '../components/TinderCard';
 import users from '../../assets/data/users';
 import AnimatedStack from '../components/AnimatedStack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { Directions } from "react-native-gesture-handler";
+
 
 const HomeScreen = () => {
 
@@ -22,6 +27,23 @@ const HomeScreen = () => {
         onSwipeLeft={onSwipeLeft}
         onSwipeRight={onSwipeRight}
       />
+      <View style={styles.icons}>
+        <View style={styles.button}>
+            <FontAwesome name="undo" size={30} color="#FBD88B" />   
+        </View>
+        <View style={styles.button}>
+            <Entypo name="cross" size={30} color="#F76C6B" />
+        </View>
+        <View style={styles.button}>
+            <FontAwesome name="star" size={30} color="#3AB4CC" />
+        </View>
+        <View style={styles.button}>
+            <FontAwesome name="heart" size={30} color="#4FCC94" />
+        </View>
+        <View style={styles.button}>
+            <Ionicons name="flash" size={30} color="#A65CD2" />
+        </View> 
+      </View>
     </View>
  );
 };
@@ -30,8 +52,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
      alignItems: 'center',
       flex: 1,
-      width: '100%'
+      width: '100%',
+      backgroundColor: '#ededed',
     },
+    icons: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+        padding: 10, 
+    },
+    button: {
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 50,
+    }
 });
 
 export default HomeScreen;
